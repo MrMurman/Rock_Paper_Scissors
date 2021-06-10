f5bf278096c1da529184aaac0eee016bbd3e38fa
MrMurman <storm2150@yandex.ru>
2021-06-10 21:30:47 +0300

    v 1.0 - app completed

--- GameState.swift
+++ GameState.swift
@@ -10,5 +10,17 @@ import Foundation
 enum GameState {
     case start, win, lose, draw
     
+    var labelText: String {
+        switch self {
+        case .start:
+            return "Game started"
+        case .win:
+            return "You won!"
+        case .lose:
+            return "You lost. Maybe next time"
+        case .draw:
+            return "Wow, what a surprise. Another round!"
+        }
+    }
     
 }
--- Rock_Paper_Scissors/ViewController.swift
+++ Rock_Paper_Scissors/ViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var appSign: UILabel!
-    @IBOutlet weak var gameStatus: UILabel!
+    @IBOutlet weak var appSignLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
     
     @IBOutlet weak var leftSignBTN: UIButton!
     @IBOutlet weak var centerSignBTN: UIButton!
@@ -22,24 +22,61 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        updateUI(gameState: .start)
     }
 
     
     func updateUI(gameState: GameState) {
         
+        statusLabel.text = gameState.labelText
+        
+        switch gameState {
+        case .start:
+            view.backgroundColor = UIColor.cyan
+            appSignLabel.text = "🤖"
+            
+            playAgainBTN.isHidden = true
+            leftSignBTN.isHidden = false
+            centerSignBTN.isHidden = false
+            rightSignBTN.isHidden = false
+            
+        case .win:
+            view.backgroundColor = UIColor.green
+        case .lose:
+            view.backgroundColor = UIColor.red
+        case .draw:
+            view.backgroundColor = UIColor.yellow
+        }
     }
     
     
+    func play(sign: Sign) {
+        let appSign = randomSign()
+        updateUI(gameState: sign.doesItBeat(anotherSign: appSign))
+        appSignLabel.text = appSign.emoji
+        
+        leftSignBTN.isHidden = true
+        centerSignBTN.isHidden = true
+        rightSignBTN.isHidden = true
+        
+        playAgainBTN.isHidden = false
+    }
+    
     @IBAction func leftSignBTNPressed(_ sender: UIButton) {
+        play(sign: .rock)
     }
     
     @IBAction func centerSignBTNPressed(_ sender: UIButton) {
+        play(sign: .paper)
     }
     
     @IBAction func rightSignBTNPressed(_ sender: UIButton) {
+        play(sign: .scissors)
     }
     
     @IBAction func playAgainBTNPressed(_ sender: UIButton) {
+        updateUI(gameState: .start)
     }
     
 }
