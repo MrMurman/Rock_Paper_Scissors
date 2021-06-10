3f1c7a07c5e4e13ab94c6c4a63b1347b96897583
MrMurman <storm2150@yandex.ru>
2021-06-10 18:36:50 +0300

    Initial Commit

---
+++ Rock_Paper_Scissors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rock_Paper_Scissors
+//
+//  Created by Андрей Бородкин on 10.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
